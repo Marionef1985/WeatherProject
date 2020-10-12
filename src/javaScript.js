66f0247d86a40f859f18c06934d273478351dc0e
Marionef1985 <marionef1985@gmail.com>
2020-10-12 19:25:09 +0200

    erase test

--- src/javaScript.js
+++ src/javaScript.js
@@ -84,10 +84,7 @@ function showTemperature(response) {
   let currentCity = response.data.name;
   let currentCityName = document.querySelector("#current-city");
   currentCityName.innerHTML = `${currentCity}`;
-  let localWeatherIconDescription = document.querySelector("#icon-id");
-  let localWeatherIcon = response.data.weather[0].icon;
-localWeatherIconDescription.innerHTML =`${localWeatherIcon}`;
-console.log(localWeatherIcon);
+  
   //
   let localWeatherDescription = response.data.weather[0].main;
   let localRemoteWeatherDescription = document.querySelector(
