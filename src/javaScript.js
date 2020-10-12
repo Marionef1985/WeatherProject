2943fdd3b8b38aa2e0cd96f083cba4b246920ab2
Marionef1985 <marionef1985@gmail.com>
2020-10-12 19:14:12 +0200

    looking for solution to show icon weather description

--- src/javaScript.js
+++ src/javaScript.js
@@ -84,7 +84,10 @@ function showTemperature(response) {
   let currentCity = response.data.name;
   let currentCityName = document.querySelector("#current-city");
   currentCityName.innerHTML = `${currentCity}`;
-  //
+  let localWeatherIconDescription = document.querySelector("#icon-weather-current");
+  let localWeatherIcon = reponse.data.weather[0].icon;
+localWeatherIconDescription.innerHTML =`${localWeatherIcon}`;
+
   //
   let localWeatherDescription = response.data.weather[0].main;
   let localRemoteWeatherDescription = document.querySelector(
