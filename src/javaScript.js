d9e451581af0c35226e87c1176defcf20cb952cc
Marionef1985 <marionef1985@gmail.com>
2020-10-22 14:07:09 +0200

    Update javaScript.js

--- src/javaScript.js
+++ src/javaScript.js
@@ -26,13 +26,13 @@ function runtime() {
 
 runtime();
 
-let dayTwo = days[(now.getDay()+2)-7];
+let dayTwo = days[(now.getDay()+2)-6];
 document.querySelector("#day-two").innerHTML = `${dayTwo}`;
-let dayThree = days[(now.getDay() +3)-7];
+let dayThree = days[(now.getDay() +3)-6];
 document.querySelector("#day-three").innerHTML = `${dayThree}`;
-let dayFour = days[(now.getDay() + 4)-7];
+let dayFour = days[(now.getDay() + 4)-6];
 document.querySelector("#day-four").innerHTML = `${dayFour}`;
-let dayFive = days[(now.getDay() + 5)-7];
+let dayFive = days[(now.getDay() + 5)-6];
 document.querySelector("#day-five").innerHTML=`${dayFive}`;
 // esta formula pide que la info que pongamos en el search tenga un accion y llame a la funcion de "get city"
 
